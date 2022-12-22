87380cdc758df250e737a0b1f0c8919ec70a2b48
Vasileios Baltas <baltasvasileios@gmail.com>
2022-12-22 23:29:12 +0200

    Update Custom_Transformers.py

--- models/Custom_Transformers.py
+++ models/Custom_Transformers.py
@@ -23,3 +23,29 @@ class MessageTransformer(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
 
         return self.message_transformer.fit_transform(X.squeeze()).toarray()
+    
+    
+    
+    class MessageTransformer_2(BaseEstimator, TransformerMixin):
+    """ Applies CountVectorizer and TfidfTransformer to strings
+
+    """
+
+    def __init__(self, tokenize):
+        self.tokenize = tokenize
+        self.message_transformer = Pipeline([
+            ('vect', CountVectorizer(tokenizer=self.tokenize)),
+            ('tfidf', TfidfTransformer())
+        ])
+
+    def fit(self, X, y=None):
+        X = X.copy()
+        self.message_transformer.fit(X)
+        return self
+
+
+    def transform(self, X, y=None):
+
+        X = X.copy()
+        return self.message_transformer.transform(X.squeeze()).toarray()
+
